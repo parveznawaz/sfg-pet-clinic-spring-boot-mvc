bbec52f00eed2ba98e7570f8329e7200083a3466
Parvez Nawaz <ParvezN@gs1ca.org>
2021-02-06 15:28:32 -0500

    Refactored Map Impl to abstract ID generation. Closes #23

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -21,9 +21,8 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
-        return super.save(object.getId(), object);
+        return super.save(object);
     }
-
     @Override
     public void delete(Vet object) {
         super.delete(object);
