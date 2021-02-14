d322ab69d6875fdb91b34c8bd6eb0be2f9d882c2
Parvez Nawaz <ParvezN@gs1ca.org>
2021-02-14 00:07:31 -0500

    Convert Vets to JPA Entities #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -1,11 +1,22 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity
+@Table("visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @OneToOne
     private Pet pet;
 
     public LocalDate getDate() {
